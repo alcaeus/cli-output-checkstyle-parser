f3415c963c3d48ef7caf244189b77120a04923f1
Andreas Braun <git@alcaeus.org>
2017-03-14 08:45:47 +0100

    Fix parsing of multiline and file-level errors

--- tests/Parser/PhpStanTest.php
+++ tests/Parser/PhpStanTest.php
@@ -25,4 +25,38 @@ final class PhpStanTest extends TestCase
 
         self::assertEquals($expectedViolation, $fileList[0]->getViolations()[0]);
     }
+
+    public function testParseMultilineError()
+    {
+        $output = file_get_contents(__DIR__ . '/../files/phpstan-cli-output-multiline.txt');
+
+        $fileList = (new PhpStan())->parse($output);
+
+        self::assertCount(1, $fileList);
+
+        self::assertSame('src/AppBundle/Document/User.php', $fileList[0]->getName());
+
+        self::assertCount(1, $fileList[0]->getViolations());
+
+        $expectedViolation = new Violation(1581, 1, 'Return typehint of method AppBundle\Document\User::getEmailAliasMappings() has invalid type AppBundle\Document\EmailAliasMapping.', 'error', 'phpstan');
+
+        self::assertEquals($expectedViolation, $fileList[0]->getViolations()[0]);
+    }
+
+    public function testParseFileLevelError()
+    {
+        $output = file_get_contents(__DIR__ . '/../files/phpstan-cli-output-file-level.txt');
+
+        $fileList = (new PhpStan())->parse($output);
+
+        self::assertCount(1, $fileList);
+
+        self::assertSame('tests/AppBundle/Document/User/LegalInformation/PaymentTest.php', $fileList[0]->getName());
+
+        self::assertCount(1, $fileList[0]->getViolations());
+
+        $expectedViolation = new Violation(1, 1, 'Class Tests\AppBundle\Document\User\PaymentTest was not found while trying to analyse it - autoloading is not probably configured properly.', 'error', 'phpstan');
+
+        self::assertEquals($expectedViolation, $fileList[0]->getViolations()[0]);
+    }
 }
